03370bc49f48a4bbe4c321325f4f15295b958db1
fatalis29 <afrizalhanif727@gmail.com>
2025-08-27 12:30:59 +0700

    Update compileSdk & targetSdk ke 34 untuk AGP 7.4.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,39 +1,47 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android") // ganti dari 'kotlin-android' ke nama resmi
-    // Flutter Gradle Plugin harus di-load terakhir
+    id("kotlin-android")
+    // Tambahkan plugin Flutter
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.dbank"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
+    namespace = "com.example.dbank_modern" // Ganti sesuai namespace project
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.dbank"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        applicationId = "com.example.dbank_modern" // Ganti sesuai ID app
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 }
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.10")
+    // Tambahkan dependency tambahan di sini jika perlu
 }
